ee7ddc1cb1d42b08122835e9a546a67e3bd64e8f
CesarDan <cdeleonc4@miumg.edu.gt>
2023-11-28 15:13:24 -0600

    Se creo un nuevo componente de formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,44 @@
+
+<script setup>
+
+
+</script>
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+    
+        <p class="text-lg mt-5 text-center mb-10">
+            Añade Pacientes y
+            <span class="text-indigo-600 font-bold">Adminístralos</span>
+        </p>
+
+        <form
+        class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        >
+
+        <div class="mb-5">
+
+            <label 
+                for="mascota"
+                class="block text-gray-700 uppercase font-bold"
+            >
+                Nombre Mascota
+            </label>
+
+            <input 
+                id="mascota"
+                type="text"
+                placeholder="Nombre de la mascota"
+                class="border-2 w-full p-2 mt-2"
+            />
+
+
+        </div>
+    
+        </form>
+
+    </div>
+ 
+</template>
+
+
