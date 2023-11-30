a3d144c763305a37792c2fdb73b11b71bfb26850
CesarDan <cdeleonc4@miumg.edu.gt>
2023-11-30 12:27:40 -0600

    Se añadio y agregó el componente de Formulario y Alerta

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,7 +1,29 @@
 
 <script setup>
-    import {ref} from 'vue'
-    const nombre = ref('')
+    import {reactive} from 'vue'
+    import Alerta from './Alerta.vue'
+
+    const alerta = reactive({
+        tipo: '',
+        mensaje: ''
+    })
+
+    const paciente = reactive({
+        nombre: '',
+        propietario: '',
+        email: '',
+        alta:'',
+        sintomas:''
+    })
+
+    const validar = () => {
+        if(Object.values(paciente).includes('')){
+            alerta.mensaje = 'Todos los campos son obligatorios'
+            alerta.tipo = 'error'
+            return
+        }  
+        
+    }
 
 </script>
 <template>
@@ -13,11 +35,17 @@
             <span class="text-indigo-600 font-bold">Adminístralos</span>
         </p>
 
+        <Alerta
+            v-if="alerta.mensaje"
+            :alerta="alerta"
+        />
+
         <form
         class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        @submit.prevent="validar"
         >
 
-        {{ nombre }}
+        {{ paciente.nombre }}
         <div class="mb-5">
 
             <label 
@@ -32,7 +60,7 @@
                 type="text"
                 placeholder="Nombre de la mascota"
                 class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                v-model="nombre"
+                v-model="paciente.nombre"
                 />
 
 
@@ -52,8 +80,7 @@
             type="text"
             placeholder="Nombre del propietario"
             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-           
-        
+            v-model="paciente.propietario"
             />
 
 
@@ -74,7 +101,8 @@
             type="email"
             placeholder="Email del propietario"
             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-        />
+            v-model="paciente.email"
+            />
 
 
         </div>
@@ -93,7 +121,8 @@
                 type="date"
                 placeholder="Email del propietario"
                 class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-            />
+                v-model="paciente.alta"
+                />
 
 
             </div>
@@ -111,12 +140,14 @@
                 id="sintomas"
                 placeholder="Describe los síntomas"
                 class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-            />
+                v-model="paciente.sintomas"
+                />
 
             <input 
                 type="submit"
                 class="mt-3 bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor pointer transition-colors"
                 value="Registrar Paciente"
+             
                 />
 
             </div>
